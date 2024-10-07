d9f97ae38cd873e985d9cc99196e0b0b7ddb653a
buithao <thaob2014700@student.ctu.edu.vn>
2024-10-07 15:27:30 +0700

    first commit

--- src/controllers/homepageController.js
+++ src/controllers/homepageController.js
@@ -1,7 +1,35 @@
+import homepageService from "../services/homepageService";
+require("dotenv").config();
+
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+
 let getHomepage = (req, res) => {
-    return res.render("homepage.ejs");
+    let fbPageId = process.env.PAGE_ID;
+    return res.render("homepage.ejs", {
+        fbPageId
+    });
+};
+
+let getFacebookUserProfile = (req, res) => {
+    return res.render("profile.ejs");
+};
+
+let setUpUserFacebookProfile = async (req, res) => {
+    // Send the HTTP request to the Messenger Platform
+    try {
+        await homepageService.setUpMessengerPlatform(PAGE_ACCESS_TOKEN);
+        return res.status(200).json({
+            message: "OK"
+        });
+    } catch (e) {
+        return res.status(500).json({
+            "message": "Error from the node server"
+        })
+    }
 };
 
 module.exports = {
-    getHomepage: getHomepage
+    getHomepage: getHomepage,
+    getFacebookUserProfile: getFacebookUserProfile,
+    setUpUserFacebookProfile: setUpUserFacebookProfile
 };
