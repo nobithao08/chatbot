36ee4d2e415c0074276df5b1276f17ff995f268f
buithao <thaob2014700@student.ctu.edu.vn>
2024-10-10 22:19:05 +0700

    commit m

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -1,60 +1,23 @@
-import request from "request";
-import moment from "moment";
-import chatBotService from "../services/chatBotService";
+// /src/controllers/chatBotController.js
+import request from 'request';
 
-let user = {
-    name: "",
-    phoneNumber: "",
-    time: "",
-    createdAt: ""
-};
+let getWebhook = (req, res) => {
+    let VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 
-// Xử lý tin nhắn từ người dùng
-let handleMessage = async (sender_psid, received_message) => {
-    let message = received_message.text.toLowerCase(); // Chuyển chữ thành dạng thường
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
 
-    if (message.includes("đặt lịch")) {
-        await chatBotService.sendTypingOn(sender_psid);
-        await chatBotService.sendMessage(sender_psid, "Bạn muốn đặt lịch khám, vui lòng chọn thời gian:");
-        // Điều hướng người dùng đến trang đặt lịch
-        await chatBotService.sendRedirectToBookingPage(sender_psid);
-    }
-    else if (message.includes("bác sĩ")) {
-        await chatBotService.sendTypingOn(sender_psid);
-        await chatBotService.sendMessage(sender_psid, "Bạn đang tìm thông tin bác sĩ, đây là danh sách:");
-        // Điều hướng người dùng đến trang thông tin bác sĩ
-        await chatBotService.sendRedirectToDoctorInfoPage(sender_psid);
-    }
-    else if (message.includes("hướng dẫn")) {
-        await chatBotService.sendTypingOn(sender_psid);
-        await chatBotService.sendMessage(sender_psid, "Đây là hướng dẫn sử dụng bot của chúng tôi:");
-        // Điều hướng đến trang hướng dẫn
-        await chatBotService.sendRedirectToGuidePage(sender_psid);
-    }
-    else {
-        await chatBotService.sendTypingOn(sender_psid);
-        await chatBotService.sendMessage(sender_psid, "Xin lỗi, tôi không hiểu yêu cầu của bạn.");
+    if (mode && token) {
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
+        } else {
+            res.sendStatus(403);
+        }
     }
 };
 
-// Xử lý postback từ người dùng
-let handlePostback = async (sender_psid, received_postback) => {
-    let payload = received_postback.payload;
-
-    switch (payload) {
-        case "CONFIRM_APPOINTMENT":
-            await chatBotService.sendTypingOn(sender_psid);
-            user.createdAt = moment(Date.now()).format('MM/DD/YYYY h:mm A');
-            await chatBotService.sendMessage(sender_psid, "Lịch khám của bạn đã được xác nhận!");
-            // Gửi thông báo đến hệ thống
-            await chatBotService.sendNotificationToTelegram(user);
-            break;
-        default:
-            console.log("Postback không xác định");
-    }
-};
-
-// Xử lý webhook
 let postWebhook = (req, res) => {
     let body = req.body;
 
@@ -76,24 +39,50 @@ let postWebhook = (req, res) => {
     }
 };
 
-let getWebhook = (req, res) => {
-    let VERIFY_TOKEN = process.env.MY_VERIFY_TOKEN;
+let handleMessage = (sender_psid, received_message) => {
+    let response;
 
-    let mode = req.query['hub.mode'];
-    let token = req.query['hub.verify_token'];
-    let challenge = req.query['hub.challenge'];
-
-    if (mode && token) {
-        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-            console.log('WEBHOOK_VERIFIED');
-            res.status(200).send(challenge);
-        } else {
-            res.sendStatus(403);
-        }
+    if (received_message.text) {
+        response = { text: `You sent the message: "${received_message.text}".` };
     }
+
+    sendMessage(sender_psid, response);
+};
+
+let handlePostback = (sender_psid, received_postback) => {
+    let response;
+    let payload = received_postback.payload;
+
+    if (payload === 'yes') {
+        response = { text: 'Thanks!' };
+    } else if (payload === 'no') {
+        response = { text: 'Oops, try sending another message.' };
+    }
+
+    sendMessage(sender_psid, response);
+};
+
+let sendMessage = (sender_psid, response) => {
+    let request_body = {
+        recipient: { id: sender_psid },
+        message: response
+    };
+
+    request({
+        uri: 'https://graph.facebook.com/v6.0/me/messages',
+        qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+        method: 'POST',
+        json: request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('Message sent!');
+        } else {
+            console.error('Unable to send message:', err);
+        }
+    });
 };
 
 export default {
-    postWebhook: postWebhook,
-    getWebhook: getWebhook
+    getWebhook,
+    postWebhook
 };
