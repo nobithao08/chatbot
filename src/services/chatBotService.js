8c323a1d203948388eceee4987536c5343a84912
buithao <thaob2014700@student.ctu.edu.vn>
2024-10-12 15:52:19 +0700

    commit

--- src/services/chatBotService.js
+++ src/services/chatBotService.js
@@ -1,48 +1,280 @@
-import request from 'request';
+require("dotenv").config();
+import request from "request";
+import homepageService from "./homepageService";
+import templateMessage from "./templateMessage";
 
-// Hàm gửi tin nhắn
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const SECONDARY_RECEIVER_ID = process.env.SECONDARY_RECEIVER_ID;
+const PRIMARY_RECEIVER_ID = process.env.FACEBOOK_APP_ID;
+
+let sendMessageWelcomeNewUser = (sender_psid) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let username = await homepageService.getFacebookUsername(sender_psid);
+            //send text message
+            let response1 = {
+                "text": `Hi ${username}! Welcome to Tech Shop Haryphamdev, where you will find what you need.`
+            };
+
+            //send an image
+            let response2 = {
+                "attachment": {
+                    "type": "image",
+                    "payload": {
+                        "url": "https://bit.ly/imageWelcome"
+                    }
+                }
+            };
+
+            let response3 = {
+                "text": "At any time, use the menu below to navigate through the features."
+            };
+
+            //send a quick reply
+            let response4 = {
+                "text": "What can I do to help you today?",
+                "quick_replies": [
+                    {
+                        "content_type": "text",
+                        "title": "Categories",
+                        "payload": "CATEGORIES",
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Lookup Order",
+                        "payload": "LOOKUP_ORDER",
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Talk to an agent",
+                        "payload": "TALK_AGENT",
+                    },
+                ]
+            };
+
+            await sendMessage(sender_psid, response1);
+            await sendMessage(sender_psid, response2);
+            await sendMessage(sender_psid, response3);
+            await sendMessage(sender_psid, response4);
+            resolve("done");
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
 let sendMessage = (sender_psid, response) => {
-    let request_body = {
-        recipient: { id: sender_psid },
-        message: response
-    };
+    return new Promise(async (resolve, reject) => {
+        try {
+            await homepageService.markMessageRead(sender_psid);
+            await homepageService.sendTypingOn(sender_psid);
+            // Construct the message body
+            let request_body = {
+                "recipient": {
+                    "id": sender_psid
+                },
+                "message": response
+            };
 
-    request({
-        uri: 'https://graph.facebook.com/v6.0/me/messages',
-        qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
-        method: 'POST',
-        json: request_body
-    }, (err, res, body) => {
-        if (!err) {
-            console.log('Tin nhắn đã được gửi!');
-        } else {
-            console.error('Không thể gửi tin nhắn:', err);
+            // Send the HTTP request to the Messenger Platform
+            request({
+                "uri": "https://graph.facebook.com/v6.0/me/messages",
+                "qs": { "access_token": PAGE_ACCESS_TOKEN },
+                "method": "POST",
+                "json": request_body
+            }, (err, res, body) => {
+                if (!err) {
+                    resolve('message sent!')
+                } else {
+                    reject("Unable to send message:" + err);
+                }
+            });
+        } catch (e) {
+            reject(e);
         }
     });
 };
 
-// Hiển thị trạng thái đang gõ
-let sendTypingOn = (sender_psid) => {
-    let request_body = {
-        recipient: { id: sender_psid },
-        sender_action: 'typing_on'
-    };
+let requestTalkToAgent = (sender_psid) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            //send a text message
+            let response1 = {
+                "text": "Ok. Someone real will be with you in a few minutes ^^"
+            };
 
-    request({
-        uri: 'https://graph.facebook.com/v6.0/me/messages',
-        qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
-        method: 'POST',
-        json: request_body
-    }, (err, res, body) => {
-        if (!err) {
-            console.log('Typing on...');
-        } else {
-            console.error('Không thể gửi trạng thái gõ:', err);
+            await sendMessage(sender_psid, response1);
+
+            //change this conversation to page inbox
+            let app = "page_inbox"
+            await passThreadControl(sender_psid, app);
+            resolve("done");
+        } catch (e) {
+            reject(e);
         }
     });
 };
 
-export default {
-    sendMessage,
-    sendTypingOn
+let passThreadControl = (sender_psid, app) => {
+    return new Promise((resolve, reject) => {
+        try {
+            let target_app_id = "";
+            let metadata = "";
+
+            if (app === "page_inbox") {
+                target_app_id = SECONDARY_RECEIVER_ID;
+                metadata = "Pass thread control to inbox chat";
+            }
+            if (app === "primary") {
+                target_app_id = PRIMARY_RECEIVER_ID;
+                metadata = "Pass thread control to the bot, primary app";
+            }
+            // Construct the message body
+            let request_body = {
+                "recipient": {
+                    "id": sender_psid
+                },
+                "target_app_id": target_app_id,
+                "metadata": metadata
+            };
+
+            // Send the HTTP request to the Messenger Platform
+            request({
+                "uri": "https://graph.facebook.com/v6.0/me/pass_thread_control",
+                "qs": { "access_token": PAGE_ACCESS_TOKEN },
+                "method": "POST",
+                "json": request_body
+            }, (err, res, body) => {
+                console.log(body)
+                if (!err) {
+                    resolve('message sent!')
+                } else {
+                    reject("Unable to send message:" + err);
+                }
+            });
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+let sendCategories = (sender_psid) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            //send a generic template message
+            let response = templateMessage.sendCategoriesTemplate();
+            await sendMessage(sender_psid, response);
+            resolve("done");
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+let sendLookupOrder = (sender_psid) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let response = templateMessage.sendLookupOrderTemplate();
+            await sendMessage(sender_psid, response);
+            resolve("done");
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+let showHeadphones = (sender_psid) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let response = templateMessage.sendHeadphonesTemplate();
+            await sendMessage(sender_psid, response);
+            resolve("done");
+        } catch (e) {
+            reject(e);
+        }
+    })
+};
+
+let showTVs = (sender_psid) => {
+    return new Promise((resolve, reject) => {
+        try {
+            resolve("done");
+        } catch (e) {
+            reject(e);
+        }
+    })
+};
+
+let showPlaystation = (sender_psid) => {
+    return new Promise((resolve, reject) => {
+        try {
+            resolve("done");
+        } catch (e) {
+            reject(e);
+        }
+    })
+};
+
+let backToCategories = (sender_psid) => {
+    sendCategories(sender_psid)
+};
+
+let backToMainMenu = (sender_psid) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let response = templateMessage.backToMainMenuTemplate();
+            await sendMessage(sender_psid, response);
+            resolve("done");
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+let takeControlConversation = (sender_psid) => {
+    return new Promise((resolve, reject) => {
+        try {
+            // Construct the message body
+            let request_body = {
+                "recipient": {
+                    "id": sender_psid
+                },
+                "metadata": "Pass this conversation from page inbox to the bot - primary app"
+            };
+
+            // Send the HTTP request to the Messenger Platform
+            request({
+                "uri": "https://graph.facebook.com/v6.0/me/take_thread_control",
+                "qs": { "access_token": PAGE_ACCESS_TOKEN },
+                "method": "POST",
+                "json": request_body
+            }, async (err, res, body) => {
+                if (!err) {
+                    //send messages
+                    await sendMessage(sender_psid, { "text": "The super bot came back !!!" });
+                    await backToMainMenu(sender_psid);
+                    resolve('message sent!')
+                } else {
+                    reject("Unable to send message:" + err);
+                }
+            });
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+module.exports = {
+    sendMessage: sendMessage,
+    sendMessageWelcomeNewUser: sendMessageWelcomeNewUser,
+    sendCategories: sendCategories,
+    sendLookupOrder: sendLookupOrder,
+    requestTalkToAgent: requestTalkToAgent,
+    showHeadphones: showHeadphones,
+    showTVs: showTVs,
+    showPlaystation: showPlaystation,
+    backToCategories: backToCategories,
+    backToMainMenu: backToMainMenu,
+    passThreadControl: passThreadControl,
+    takeControlConversation: takeControlConversation
 };
