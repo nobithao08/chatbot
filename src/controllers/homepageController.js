bfe7e8ff7d29d3d32eac115a5e957f3bc624ba36
buithao <thaob2014700@student.ctu.edu.vn>
2024-10-15 16:22:38 +0700

    commit mm

--- src/controllers/homepageController.js
+++ src/controllers/homepageController.js
@@ -1,3 +1,53 @@
+require("dotenv").config();
+import request from "request";
+import chatBotService from "../services/chatBotService"; // Giả sử bạn có tệp service này
+
+const MY_VERIFY_TOKEN = process.env.MY_VERIFY_TOKEN;
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+
+let getHomepage = (req, res) => {
+    return res.render("homepage.ejs");
+};
+
+let getWebhook = (req, res) => {
+    let VERIFY_TOKEN = MY_VERIFY_TOKEN;
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+
+    if (mode && token) {
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
+        } else {
+            res.sendStatus(403);
+        }
+    }
+};
+
+let postWebhook = (req, res) => {
+    let body = req.body;
+
+    if (body.object === 'page') {
+        body.entry.forEach(function (entry) {
+            let webhook_event = entry.messaging[0];
+            let sender_psid = webhook_event.sender.id;
+            console.log('Sender PSID: ' + sender_psid);
+
+            if (webhook_event.message) {
+                handleMessage(sender_psid, webhook_event.message);
+            } else if (webhook_event.postback) {
+                handlePostback(sender_psid, webhook_event.postback);
+            }
+        });
+
+        res.status(200).send('EVENT_RECEIVED');
+    } else {
+        res.sendStatus(404);
+    }
+};
+
+// Xử lý tin nhắn
 let handleMessage = (sender_psid, received_message) => {
     let response;
 
@@ -5,7 +55,6 @@ let handleMessage = (sender_psid, received_message) => {
     const doctorKeywords = ["bác sĩ", "tìm bác sĩ", "xem bác sĩ"];
     const legPainKeywords = ["đau chân", "đau xương khớp", "đau cơ", "vấn đề chân"];
 
-    // Kiểm tra nếu tin nhắn chứa một trong các từ khóa đặt lịch
     if (received_message.text) {
         let messageText = received_message.text.toLowerCase();
 
@@ -44,21 +93,17 @@ let handleMessage = (sender_psid, received_message) => {
                     }]
                 }
             }
-        }
+        };
     }
 
     callSendAPI(sender_psid, response);
 };
 
-
 // Xử lý sự kiện postback
 let handlePostback = (sender_psid, received_postback) => {
     let response;
-
-    // Lấy payload của postback
     let payload = received_postback.payload;
 
-    // Thiết lập phản hồi dựa trên postback payload
     if (payload === 'yes') {
         response = { "text": "Cảm ơn bạn đã cung cấp thông tin. Hệ thống sẽ sớm xem và phản hồi bạn sau. Vui lòng chờ!" };
     } else if (payload === 'no') {
@@ -67,6 +112,57 @@ let handlePostback = (sender_psid, received_postback) => {
         response = { "text": "Xin chào, đây là trang chính thức của BookingCare with Nobi. Tôi có thể giúp gì cho bạn?" };
     }
 
-    // Gửi tin nhắn phản hồi
     callSendAPI(sender_psid, response);
+};
+
+// Gửi tin nhắn qua API
+let callSendAPI = (sender_psid, response) => {
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": response
+    };
+
+    request({
+        "uri": "https://graph.facebook.com/v6.0/me/messages",
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('Message sent!');
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
+};
+
+let setupProfile = (req, res) => {
+    let request_body = {
+        "get_started": { "payload": "GET_STARTED" },
+        "whitelisted_domains": ["https://chatbot-3iqe.onrender.com/"]
+    };
+
+    request({
+        "uri": `https://graph.facebook.com/v9.0/me/messenger_profile`,
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('Setup user profile succeeds!');
+        } else {
+            console.error("Unable to setup user profile: " + err);
+        }
+    });
+
+    return res.send('Setup user profile succeeds!');
+};
+
+module.exports = {
+    getHomepage,
+    getWebhook,
+    postWebhook,
+    setupProfile,
 };
